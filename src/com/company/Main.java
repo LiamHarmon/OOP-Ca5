30300c77f7e70aac3d573bbab08d05257199c5ee
liamh <d00236258@student.dkit.ie>
2022-03-05 12:02:26 +0000

    Added Player HashMap Section 3.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,7 @@
 package com.company;
 
 import java.io.IOException;
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -58,7 +55,7 @@ public class Main {
                         break;
                     case HASHMAP:
                         System.out.println("HashMap option chosen");
-
+                        HashMap();
                         break;
                     case TREEMAP:
                         System.out.println("TreeMap option chosen");
@@ -97,11 +94,36 @@ public static void ArrayList1()
     playerList.add(new Player("Peter O'Mahony","Back Row",82,244.00));
     playerList.add(new Player("Johnny Sexton","Flyhalf",103,216.00));
 
+    System.out.printf("%5s %15s %15s %20s %n", "Name", "Position", "Caps", "Total Time");
 
-
-    for(Player ireland : playerList)
+    for(Player player : playerList)
     {
-        System.out.println(ireland);
+        System.out.println(player);
+    }
+}
+
+public static void HashMap()
+{
+    Map<Integer, Player> playerMap = new HashMap<>();
+
+    playerMap.put(1, new Player("Bundee Aki","Centre",35,281.00));
+    playerMap.put(2, new Player("Joey Carbery","Flyhalf",30,262.00));
+    playerMap.put(3, new Player("Keith Earls","Fullback/Wing",96,36.00));
+    playerMap.put(4, new Player("Tadhg Furlong","Prop",55,362.00));
+    playerMap.put(5, new Player("Cian Healy","Prop",114,73.00));
+    playerMap.put(6, new Player("Iain Henderson","Lock/Back Row",66,93.00));
+    playerMap.put(7, new Player("Robbie Henshaw","Centre/Fullback",55,150.00));
+    playerMap.put(8, new Player("Conor Murray","Scrumhalf",94,110.00));
+    playerMap.put(9, new Player("Peter O'Mahony","Back Row",82,244.00));
+    playerMap.put(10,new Player("Johnny Sexton","Flyhalf",103,216.00));
+
+    Set<Integer> keySet = playerMap.keySet();
+
+    for (Integer key : keySet) {
+        Player player = playerMap.get(key);
+        System.out.println("Player_ID: " + key + " Name: "
+                + player.getName() + " " +
+                " " + "Position: " + player.getPosition() + " " + "Caps: " + player.getCaps() + " " + "Total Time: " + player.getTotalTime());
     }
 }
 
