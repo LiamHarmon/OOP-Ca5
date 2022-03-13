df4eb823725e6362e56b7f127bbecffc995e0591
liamh <d00236258@student.dkit.ie>
2022-03-13 20:13:03 +0000

    Added Section 7.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Objects;
 
-public class Player
+public class Player implements Comparable<Player>
 {
     String name;
     String position;
@@ -55,5 +55,27 @@ public class Player
     @Override
     public int hashCode() {
         return Objects.hash(getName(), getPosition(), getCaps(), getTotalTime());
+    }
+
+    public int compareTo(Player p)
+    {
+        boolean bSamePlayerName =
+                this.getName().equalsIgnoreCase(p.getName());
+
+        boolean bSamePlayerPosition =
+                this.getPosition().equalsIgnoreCase(p.getPosition());
+
+        if(bSamePlayerName && bSamePlayerPosition)
+        {
+            return this.getCaps() - p.getCaps();
+        }
+        else if(!bSamePlayerName && bSamePlayerPosition)
+        {
+            return this.getName().compareToIgnoreCase(p.getName());
+        }
+        else
+        {
+            return this.getPosition().compareToIgnoreCase(p.getPosition());
+        }
     }
 }
